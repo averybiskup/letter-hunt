8b92d329c5a605eefa46f1cb8fea6451189537e3
Avery Biskup <averybiskup@gmail.com>
2022-07-27 18:59:05 -0700

    Socket ready to listen to client

--- server/server.go
+++ server/server.go
@@ -10,16 +10,36 @@ const PORT = 8080
 
 func main() {
 
-  fmt.Printf("Starting server on port %d", PORT)
+  fmt.Printf("Starting server on port %d\n", PORT)
 
   ServePort := fmt.Sprintf(":%d", PORT)
 
-  ln, _ := net.Listen("tcp", ServePort)
+  ln, err := net.Listen("tcp", ServePort)
 
-  conn, _ := ln.Accept()
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  defer ln.Close()
 
   for {
-    message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Print("Message Received:", string(message))
+    conn, _ := ln.Accept()
+    go handleConnection(conn)
   }
+}
+
+func handleConnection(con net.Conn) {
+  for {
+    data, err := bufio.NewReader(con).ReadString('\n')
+
+    if (err != nil) {
+      fmt.Println(err)
+      return
+    }
+
+    fmt.Println(data)
+  }
+
+  con.Close()
 }
